84b5161d063e90e871068c943bdce3ddead663d3
Furqan Ul haq <mfurqanulhaq1997@gmail.com>
2018-11-17 14:07:06 +0500

    Initial Commit

---
+++ MassartTech_Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MassartTech_Task
+//
+//  Created by FURQAN on 11/17/18.
+//  Copyright © 2018 FURQAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
